5e4b220e18f10604acd3b3c2c1f4f7d43427776e
Seyteru <s.cioffi2001@gmail.com>
2024-04-09 00:26:42 +0200

    Commit Test

--- exercise1/container/traversable.cpp
+++ exercise1/container/traversable.cpp
@@ -1,6 +1,16 @@
 
 namespace lasd {
 
-    
+    template <typename Data>
+    bool Exists(Data val){
+        bool exists = false;
+        Traverse(
+            [val, &exists](const Data &data){
+                exists = exists || (data == val)
+            }
+        );
+        return exists;
+        
+    }
 
 }
